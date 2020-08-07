43ec65f43d0c5066e097961f55682d1df5b514ac
Pronoy4496 <67531767+Pronoy4496@users.noreply.github.com>
2020-08-07 21:49:39 +0600

    nullvsUndefined

---
+++ nullVsUndefined.js
@@ -0,0 +1,12 @@
+// let pakhi;
+// console.log(pakhi);
+// function add (num1, num2){
+//     console.log(num1+num2);
+// }
+// function add (num1, num2){
+//     console.log(num2);
+// }
+// const result = add(13,18);
+// console.log(result);
+let fun = undefined;
+console.log(fun);
